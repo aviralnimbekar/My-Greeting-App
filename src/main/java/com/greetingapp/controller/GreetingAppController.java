a34211cd4e89b40cbc6d48164e40aa1bae67f135
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-10-09 12:54:19 +0530

    ADDED : Service layer and Welcome Message

--- src/main/java/com/greetingapp/controller/GreetingAppController.java
+++ src/main/java/com/greetingapp/controller/GreetingAppController.java
@@ -1,10 +1,20 @@
 package com.greetingapp.controller;
 
+import com.greetingapp.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("greet")
+@RequestMapping("/greet")
 public class GreetingAppController {
 
+    @Autowired
+    private GreetingService greetingService;
+
+    @GetMapping({"", "/", "/hello"})
+    public String getWelcomeMessage() {
+        return greetingService.getHelloMessage();
+    }
 }
